d69712700ca845563e1ce024910a62df2be98454
Ejiofor Celestine Chinaza <121381551+CyberVerse2@users.noreply.github.com>
2023-08-30 00:51:25 +0100

    added -passWithNoTests parameter to my test

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -6,7 +6,7 @@ const { loadPlanetsData } = require('../../models/planets.model');
 describe('Launches API', () => {
   beforeAll(async () => {
     await mongoConnect();
-    await loadPlanetsData();
+    // await loadPlanetsData();
   });
 
   afterAll(async () => {
