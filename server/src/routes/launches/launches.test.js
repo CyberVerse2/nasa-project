c731de86b1998fdf227ed9bf1ff744eb23d78980
Ejiofor Celestine Chinaza <121381551+CyberVerse2@users.noreply.github.com>
2023-08-30 01:30:36 +0100

    changed launches.test

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -2,11 +2,13 @@ const request = require('supertest');
 const app = require('../../app');
 const { mongoConnect, mongoDisconnect } = require('../../services/mongo');
 const { loadPlanetsData } = require('../../models/planets.model');
+const { loadLaunchData } = require('../../models/launches.model');
 
 describe('Launches API', () => {
   beforeAll(async () => {
     await mongoConnect();
-  await loadPlanetsData();
+    await loadPlanetsData();
+    await loadLaunchData();
   });
 
   afterAll(async () => {
